037ac69262cbae210c53858b4f008be8f1e9545d
Aldair Alarcon <aldoalar1992@gmail.com>
2021-11-09 23:50:39 -0500

    iniciativas

--- gzl_crm/models/crm_lead.py
+++ gzl_crm/models/crm_lead.py
@@ -6,18 +6,22 @@ class CrmLead(models.Model):
     
     _inherit = 'crm.lead'
 
-
+    is_won = fields.Boolean(related='stage_id.is_won')
+    
     def crear_adjudicado(self):
         if self.stage_id.is_won:
             monto=0
-            for l in self.quotation_count:
-                monto=+l.amount_total
+            for l in self.order_ids:
+                if l.state=='draft':
+                    monto+=l.amount_total
             self.env['res.partner'].create({
+                                        'name':self.partner_id.name,
+                                        'type':'contact',
                                         'tipo':'adjudicado',
                                         'monto':monto,
-                                        'function':self.partner_id.function or '',
-                                        'email':self.partner_id.email or '',
-                                        'phone':self.partner_id.phone or '',
-                                        'mobile':self.partner_id.mobile or '',
+                                        'function':self.partner_id.function or None,
+                                        'email':self.partner_id.email or None,
+                                        'phone':self.partner_id.phone or None,
+                                        'mobile':self.partner_id.mobile or None,
             })
             
