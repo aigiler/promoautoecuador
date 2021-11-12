50f0fc352151ad3be568e731f4806f55295e0923
Yadira Quimis <yadiraq27@gmail.com>
2021-11-12 12:15:44 -0500

    gzl_sign

---
+++ sign_gzl/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name" : "sign_gzl",
+    "version" : "0.1",
+    'depends' :['sign',
+                ],
+    "author" : "Yadira Quimis Gizlo",
+    "description" : """
+    Heredado de SIGN
+                    """,
+    "website" : "http://www.gizlocorp.com",
+    "category" : "Generic Modules",
+   
+    "data" : [   
+                'data/data_contrato_licitacion.xml',
+
+                   
+            ],
+    
+    'installable': True,
+    'auto_install': False,
+}
+
