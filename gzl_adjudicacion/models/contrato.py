c9560fb44dcc2c8e542a8ebfcb6e73f37a374b70
Yadira Quimis <yadiraq27@gmail.com>
2021-11-18 14:33:58 -0500

    contrato

--- gzl_adjudicacion/models/contrato.py
+++ gzl_adjudicacion/models/contrato.py
@@ -4,7 +4,6 @@ from odoo import api, fields, models
 
 
 class Contrato(models.Model):
-    
     _name = 'contrato'
     _description = 'Contrato'
     _rec_name = 'secuencia'
@@ -26,7 +25,7 @@ class Contrato(models.Model):
     tipo_de_contrato = fields.Many2one('tipo.contrato.adjudicado', string='Tipo de Contrato')
     codigo_grupo = fields.Char(string='Código de Grupo')
     provincias = fields.Many2one('res.country.state', string='Provincia')
-    #archivo = fields.
+    archivo = fields.Binary(string='Archivo')
     fecha_contrato = fields.Date(string='Fecha Contrato')
     plazo_meses = fields.Selection([('60', '60 Meses'), 
                                     ('72', '72 Meses')
@@ -46,11 +45,13 @@ class Contrato(models.Model):
         ], string='Estado', default='borrador')
     observacion = fields.Char(string='Observación')
     ciudad = fields.Many2one('res.country.city', string='Ciudad', domain="[('provincia_id','=',provincias)]") 
-    #archivo_adicional =
+    archivo_adicional = fields.Binary(string='Archivo Adicional')
     fecha_inicio_pago = fields.Char(string='Fecha Inicio de Pago')
     cuota_capital = fields.Monetary(string='Cuota Capital', currency_field='currency_id')
     iva_administrativo = fields.Monetary(string='Iva Administrativo', currency_field='currency_id')
-    
+    estado_de_cuenta_ids = fields.One2many('contrato.estado.cuenta', 'contrato_id')
+
+
     @api.onchange('cliente', 'grupo')
     def onchange_provincia(self):
         self.env.cr.execute("""select id from res_country_state where country_id={0}""".format(self.env.ref('base.ec').id))
@@ -67,3 +68,29 @@ class Contrato(models.Model):
         res = self.env['res.config.settings'].sudo(1).search([], limit=1, order="id desc")
         self.tasa_administrativa= res.tasa_administrativa
         self.dia_corte = res.dia_corte
+
+
+
+class ContratoEstadoCuenta(models.Model):
+    _name = 'contrato.estado.cuenta'
+    _description = 'Contrato - Tabla de estado de cuenta de Aporte'
+
+    contrato_id = fields.Many2one('contrato')
+    numero_cuota = fields.Char(String='Número de Cuota')
+    fecha = fields.Date(String='Fecha Pago')
+    fecha_pagada = fields.Date(String='Fecha Pagada')
+    currency_id = fields.Many2one('res.currency', readonly=True, default=lambda self: self.env.company.currency_id)
+    cuota_capital = fields.Monetary(string='Cuota Capital', currency_field='currency_id')
+    cuota_adm = fields.Monetary(string='Cuota Adm', currency_field='currency_id')
+    factura_id = fields.Many2one('account.move', string='Factura')
+    pago_ids = fields.Many2many('account.payment', string='Pagos')
+    seguro = fields.Monetary(string='Seguro', currency_field='currency_id')
+    rastreo = fields.Monetary(string='Rastreo', currency_field='currency_id')
+    otro = fields.Monetary(string='Otro', currency_field='currency_id')
+    monto_pagado = fields.Monetary(string='Monto Pagado', currency_field='currency_id')
+    saldo = fields.Monetary(string='Saldo', currency_field='currency_id')
+    certificado = fields.Binary(string='Certificado')
+    estado_pago = fields.Selection([('pendiente', 'Pendiente'), 
+                                      ('pagado', 'Pagado')
+                                    ],string='Estado de Pago', default='pendiente') 
+    
