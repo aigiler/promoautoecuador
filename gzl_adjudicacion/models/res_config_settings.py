998cc17f9af748dc031eb6523ef1ab6f16fa249c
Yadira Quimis <yadiraq27@gmail.com>
2021-11-18 10:58:44 -0500

    ajustes adjudicados

--- gzl_adjudicacion/models/res_config_settings.py
+++ gzl_adjudicacion/models/res_config_settings.py
@@ -2,7 +2,7 @@
 
 from odoo import api, fields, models
 
-class ResConfigSettings(models.Model):
+class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     _description = 'Ajustes de Adjudicaciones'
 
