8283a0c2956540fdc155e8c7767c59c1021716b5
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2021-11-24 11:00:45 -0500

    modulo entregaVehiculo
    añadiendo nuevo modulo

--- gzl_adjudicacion/models/__init__.py
+++ gzl_adjudicacion/models/__init__.py
@@ -2,6 +2,7 @@
 from . import adjudicado
 from . import tipo_contrato
 from . import grupo_adjudicado
+from . import entrega_vehiculo
 from . import asamblea
 from . import contrato
 from . import res_config_settings
--- gzl_adjudicacion/models/entrega_vehiculo.py
+++ gzl_adjudicacion/models/entrega_vehiculo.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
+from gzl_adjudicacion.models.contrato import Contrato
 from odoo import api, fields, models
 
 
-class Asamblea(models.Model):
-    _name = 'asamblea'
-    _description = 'Proceso de Asamblea'
+class EntregaVehiculo(models.Model):
+    _name = 'entrega.vehiculo'
+    _description = 'Entrega de vehiculo'
 
     name=fields.Char('Nombre')
+    nombre=fields.Text('Nombre / Razón social',  required=True)
+    nombre = fields.Many2one('res.partner', string="Nombre / Razón social")
+    identificacion = fields.Many2one('res.partner.', 'val')
+    # identificacion=fields.Text('Cédula de identidad',  required=True)
     descripcion=fields.Text('Descripcion',  required=True)
+    direccion=fields.Text('Dirección',  required=True)
+    grupo = fields.Many2one('grupo.adjudicado', string="Grupo")
+    tipo_contrato = fields.Many2one('res.partner.', 'tipo_contrato')
+    #contrato=fields.One2many('contrato.estado.cuenta', 'contrato_id')
     active=fields.Boolean( default=True)
-    integrantes = fields.One2many('integrante.grupo.adjudicado.asamblea','asamblea_id')
     junta = fields.One2many('junta.grupo.asamblea', 'asamblea_id')
-    fecha_inicio = fields.Datetime(String='Fecha Inicio')
-    fecha_fin = fields.Datetime(String='Fecha Fin')
     secuencia = fields.Char(index=True)
     state = fields.Selection(selection=[
             ('borrador', 'Borrador'),
-            ('en_curso', 'En Curso'),
-            ('cerrado', 'Cerrado')
+            ('cobranzas', 'Cobranzas'),
+            ('entrega_de_vehiculo', 'Entrega de vehiculo')
             ], string='Estado', copy=False, tracking=True, default='borrador')
 
     @api.model
     def create(self, vals):
-        vals['name'] = self.env['ir.sequence'].next_by_code('asamblea')
-        return super(Asamblea, self).create(vals)
+        vals['name'] = self.env['ir.sequence'].next_by_code('entrega.vehiculo')
+        return super(EntregaVehiculo, self).create(vals)
 
     # @api.onchange('grupo_id')
     # def agregar_grupo_a_asamblea(self):
@@ -48,7 +54,7 @@ class IntegrantesGrupoAsamblea(models.Model):
     #adjudicado_id = fields.Many2one('res.partner')
     #monto=fields.Float('Monto' )
     #es_ganador = fields.Boolean(String='Ganador', default=False)
-
+s
 class JuntaGrupoAsamblea(models.Model):
     _name='junta.grupo.asamblea'
 
