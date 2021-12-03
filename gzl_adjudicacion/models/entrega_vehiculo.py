d712567957c6b87b5cc7d8b6b16e47c6104d0001
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2021-12-03 11:01:16 -0500

    Update entrega_vehiculo.py

--- gzl_adjudicacion/models/entrega_vehiculo.py
+++ gzl_adjudicacion/models/entrega_vehiculo.py
@@ -191,8 +191,8 @@ class EntegaVehiculo(models.Model):
     def calcular_valor_cuotas_canceladas(self):
         for rec in self:
             rec.porcentajeTotal= 100.00
-            rec.cuotasCanceladas =37
-            rec.montoCuotasCanceladas = rec.valorCuota * rec.cuotasCanceladas
+            rec.cuotasCanceladas =31
+            rec.montoCuotasCanceladas = (rec.valorCuota * rec.cuotasCanceladas)*100
 
     @api.depends('valorCuota', 'plazoMeses')
     def calcular_valor_total_plan(self):
