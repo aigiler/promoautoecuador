867ce17f21eedf25f282597036116caf014eb691
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2021-12-08 14:22:18 -0500

    Update res_config_settings.py

--- gzl_adjudicacion/models/res_config_settings.py
+++ gzl_adjudicacion/models/res_config_settings.py
@@ -7,8 +7,25 @@ class ResConfigSettings(models.TransientModel):
     _description = 'Ajustes de Adjudicaciones'
     
     dia_corte = fields.Integer(string='Día de Corte', default=5)
-    tasa_administrativa = fields.Float(string='Tasa Administrativa %', default=4)
+    tasa_administrativa = fields.Float(string='Tasa Administrativa %', default=4.00)
     requisitosPoliticasCredito = fields.Text(string='Informacion Cobranzas')
+
+
+    @api.onchange('dia_corte')
+    def _onchange_dia_corte(self):
+        if not self.dia_corte:
+            self.dia_corte = 0.0
+
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res.update(
+            dia_corte=self.env['ir.config_parameter'].sudo().get_param('gzl_adjudicacion.dia_corte')
+        )
+        return res
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('gzl_adjudicacion.dia_corte', self.dia_corte)
 
 
 
