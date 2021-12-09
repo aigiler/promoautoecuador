6c9e81501ac6eb59e7a2d51332374ab17fa994b7
unknown <Aldair Alarcon>
2021-12-09 10:58:35 -0500

    cambios

--- gzl_adjudicacion/__manifest__.py
+++ gzl_adjudicacion/__manifest__.py
@@ -1,8 +1,7 @@
 {
     "name" : "gzl_adjudicacion",
     "version" : "0.1",
-    'depends' :['base',
-                ,'mail','portal'],
+    'depends' :['base','mail','portal'],
     "author" : "Yadira Quimis Gizlo",
     "description" : """
     product
