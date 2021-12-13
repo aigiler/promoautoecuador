d71271df167db048c745b8d01cd7991abe7f5b7a
unknown <Aldair Alarcon>
2021-12-13 13:21:37 -0500

    cambios en modulos

--- gzl_adjudicacion/models/asamblea.py
+++ gzl_adjudicacion/models/asamblea.py
@@ -117,6 +117,7 @@ class IntegrantesGrupoAsamblea(models.Model):
     adjudicado_id = fields.Many2one('res.partner', string="Nombre")
     descripcion=fields.Char('Descripcion',  )
     grupo_id = fields.Many2one('integrante.grupo.adjudicado.asamblea')
+    grupo_cliente = fields.Many2one('grupo.adjudicado')
     nro_cuota_licitar = fields.Integer(string='Nro de Cuotas a Licitar')
     carta_licitacion = fields.Selection([('si', 'Si'), ('no', 'No')], string='Carta Licitación')
     carta_doc = fields.Binary(string='Carta Licitación')
