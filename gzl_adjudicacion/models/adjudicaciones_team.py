c9ca5d72c62aa46bfe3aeac7a0dcefdbf7189943
unknown <Aldair Alarcon>
2021-12-20 15:34:38 -0500

    cambios roles

--- gzl_adjudicacion/models/adjudicaciones_team.py
+++ gzl_adjudicacion/models/adjudicaciones_team.py
@@ -10,10 +10,28 @@ from odoo.exceptions import ValidationError
 
 class AdjudicacionTeam(models.Model):
     _name = 'adjudicaciones.team'
-    _inherit = ['mail.alias.mixin', 'crm.team']
-    _description = 'Roles'
-
+    _description = 'Adjudicaciones Roles'
 
     correo = fields.Char(string="Correo")
 
+    name = fields.Char('Sales Team', required=True, translate=True)
+    sequence = fields.Integer('Sequence', default=10)
+    active = fields.Boolean(default=True, help="If the active field is set to false, it will allow you to hide the Sales Team without removing it.")
+    company_id = fields.Many2one('res.company', string='Company',
+                                 default=lambda self: self.env.company, index=True)
+    currency_id = fields.Many2one(
+        "res.currency", related='company_id.currency_id',
+        string="Currency", readonly=True)
+    user_id = fields.Many2one('res.users', string='Team Leader', check_company=True)
+    member_ids = fields.One2many(
+        'res.users', 'adjudicaciones_team_id', string='Channel Members', check_company=True,
+        domain=lambda self: [('groups_id', 'in', self.env.ref('base.group_user').id)],
+        help="Add members to automatically assign their documents to this sales team. You can only be member of one team.")
 
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    adjudicaciones_team_id = fields.Many2one(
+        'adjudicaciones.team', "User's Adjudicaciones Team")
