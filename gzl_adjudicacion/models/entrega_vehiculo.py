fa85942b46d9365deab41d10b2a627a7189843b1
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2021-12-27 16:27:18 -0500

    modificacion entrega_vehiculo

--- gzl_adjudicacion/models/entrega_vehiculo.py
+++ gzl_adjudicacion/models/entrega_vehiculo.py
@@ -619,7 +619,7 @@ class ItemPatrimonioEntregaVehiculo(models.Model):
     currency_id = fields.Many2one(
         'res.currency', readonly=True, default=lambda self: self.env.company.currency_id)
     entrega_id = fields.Many2one('entrega.vehiculo')
-    patrimonio_id = fields.Many2one('item.patrimonio')
+    patrimonio_id = fields.Many2one('items.patrimonio')
     valor  = fields.Monetary(digits=(6, 2))
 
 
