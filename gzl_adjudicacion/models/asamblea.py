d6e20ee2cdab64dbcc327a595ed7e7cb2d5e0f41
unknown <Aldair Alarcon>
2022-01-10 17:14:37 -0500

    cambios en modulo de gzl_adjudicacion

--- gzl_adjudicacion/models/asamblea.py
+++ gzl_adjudicacion/models/asamblea.py
@@ -221,7 +221,8 @@ class GanadoresAsamblea(models.Model):
     grupo_id = fields.Many2one('asamblea')
     adjudicado_id = fields.Many2one('res.partner', string="Nombre")
     contrato_id = fields.Many2one('contrato', string="Nombre")
-    monto_financiamiento = fields.Monetary(related='contrato_id.monto_financiamiento', string="Nombre")
+    monto_financiamiento = fields.Monetary(related='contrato_id.monto_financiamiento',string='Monto Financiamiento', currency_field='currency_id', track_visibility='onchange')
+
     monto_adjudicar = fields.Float( string="Nombre")
     grupo_adjudicado_id = fields.Many2one('grupo.adjudicado',string="Grupo")
     puntos = fields.Integer(string='Nro de Cuotas a Licitar')
