2af3b3558b9ba00471a05d85d85d9385b22bf257
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2022-01-10 15:27:05 -0500

    Update informe_excel.py

--- gzl_reporte/wizard/informe_excel.py
+++ gzl_reporte/wizard/informe_excel.py
@@ -72,27 +72,25 @@ def informe_credito_cobranza(ruta,lista):
 
 
     sheet = workbook['Aprobacion']
+    sheet = workbook['Liquidacion']
+    sheet = workbook['Orden Compra']
     listaSheet2 = list(filter(lambda x: (x['hoja']==2), lista)) 
+    listaSheet3 = list(filter(lambda x: (x['hoja']==3), lista)) 
+    listaSheet4 = list(filter(lambda x: (x['hoja']==4), lista)) 
 
+###########Llenar segundo sheet
     for campos in listaSheet2:
         cell = sheet.cell(row=campos['fila'], column=campos['columna'])
         cell.value = campos['valor']
 
-
-###########Llenar segundo sheet
-
-
-
+    for camposLiq in listaSheet3:
+        cell = sheet.cell(row=camposLiq['fila'], column=camposLiq['columna'])
+        cell.value = camposLiq['valor']
 
 
-
-
-
-
-
-
-
-
+    for camposOrden in listaSheet2:
+        cell = sheet.cell(row=camposOrden['fila'], column=camposOrden['columna'])
+        cell.value = camposOrden['valor']
 
     workbook.save(ruta)
 
