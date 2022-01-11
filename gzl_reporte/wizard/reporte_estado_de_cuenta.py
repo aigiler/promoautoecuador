e96a79e0c894cd6563aa4633dcb55ad837797cc1
Aldair Alarcon <aldoalar1992@gmail.com>
2022-01-11 18:18:03 -0500

    cambios en reporte estado de cuenta

--- gzl_reporte/wizard/reporte_estado_de_cuenta.py
+++ gzl_reporte/wizard/reporte_estado_de_cuenta.py
@@ -102,7 +102,7 @@ class ReporteEstadoDeCuenta(models.TransientModel):
             current_line = next(line)
             sheet.write(current_line, 0, linea.numero_cuota ,body)
             sheet.write(current_line, 1, linea.fecha, date_format)
-            sheet.write(current_line, 2, linea.fecha_pagada  , date_format)
+            sheet.write(current_line, 2, linea.fecha_pagada or ''  , date_format)
             sheet.write(current_line, 3, linea.cuota_capital , currency_format)
             sheet.write(current_line, 4, linea.cuota_adm ,currency_format)
             sheet.write(current_line, 5, linea.iva_adm ,currency_format)
