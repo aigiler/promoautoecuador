e39af0429579e96e94cdc4b27db8c0f9ff3e8e21
Aldair Alarcon <aldoalar1992@gmail.com>
2022-01-11 18:10:22 -0500

    cambios en reporte estado de cuenta

--- gzl_reporte/wizard/reporte_estado_de_cuenta.py
+++ gzl_reporte/wizard/reporte_estado_de_cuenta.py
@@ -20,130 +20,101 @@ class ReporteEstadoDeCuenta(models.TransientModel):
     _name = "reporte.estado.de.cuenta"
     #_inherit = ""
     
-    cliente_id = fields.Many2one('res.partner',string='Socio')
+    contrato_id = fields.Many2one('contrato',string='Contrato')
     
-    def obtener_contato(self, filtro, socio):
-        contrato = self.env['contrato'].search([('cliente_id','=',socio)]).ids
-        
-        estado_cuenta = fields.One2many(
-        'contrato.estado.cuenta', 'contrato_id', track_visibility='onchange')
-        
-        
-        
 
-#         partners=list(set(self.env['account.payment'].search(filtro).mapped('partner_id').mapped('id')))
-#         obj_partner=self.env['res.partner'].browse(partners)
-#         lista_partner=[]
+ 
 
-#         for partner in obj_partner:
-#             dct={}
-#             dct['id']=partner.id
-#             dct['nombre']=partner.name
-#             lista_partner.append(dct)
-
-
-
-#         return lista_partner
-
-
-
-            
-#     def obtener_listado_payment_por_empresa(self,partner_id,filtro):
-#         if partner_id:
-#             filtro.append(('partner_id','=',partner_id))            
-
-#         if self.tipo_empresa=='proveedor':
-#             filtro.append(('partner_type','=','supplier'))            
-#         else:
-#             filtro.append(('partner_type','=','customer'))            
-
-# #######facturas
-#         payments=self.env['account.payment'].search(filtro,order='name asc')
-        
-#         lista_facturas=[]
-
-#         for payment in payments:
-#             dct={}
-#             dct['numero_documento']=payment.name
-#             dct['fecha_emision']=payment.payment_date
-#             dct['fecha_vencimiento']=payment.date_to
-# ##### Calculo de pagos
-
-
-#             dct['monto_adeudado']=payment.amount_residual
-#             dct['monto_aplicado']=payment.amount - payment.amount_residual
-#             dct['monto_anticipo']=payment.amount
-
-#             dct['observaciones']=payment.communication
-
-
-
-#             lista_facturas.append(dct)
-#         return lista_facturas
-
-
-
-
-    def print_report_pdf(self):
-        return self.env.ref('gzl_reporte.repote_anticipo_pdf_id').report_action(self)
-
-
-#     def obtenerDatos(self,):
-
-#         filtro=[('payment_date','>=',self.date_from),
-#             ('payment_date','<=',self.date_to),('tipo_transaccion','=','Anticipo')]
-
-
-#         lista_partner=self.obtener_listado_partner_payment(filtro)
-#         lines=[]
+    def print_report_xls(self):
+        file_data = BytesIO()
+        workbook = xlsxwriter.Workbook(file_data)
+        name = 'Estado de Cuenta'
+        self.xslx_body(workbook, name)
         
 
-#         for partner in lista_partner:
-#             filtro=[('payment_date','>=',self.date_from),
-#                 ('payment_date','<=',self.date_to),('tipo_transaccion','=','Anticipo')]
-            
-#             lines.append({'numero_documento':partner['nombre'],'reglon':'titulo'})
+        workbook.close()
+        file_data.seek(0)
+        
 
-#             lista_anticipos=self.obtener_listado_payment_por_empresa(partner['id'],filtro)
-#             for dct in lista_anticipos:
-#                 dct['reglon']='detalle'
-#                 lines.append(dct)
-#             dctTotal={}
-#             dctTotal['numero_documento']='Total '+ partner['nombre']
-
-#             dctTotal['monto_anticipo']=round(sum(map(lambda x:x['monto_anticipo'],lista_anticipos)),2)
-#             dctTotal['monto_aplicado']=round(sum(map(lambda x:x['monto_aplicado'],lista_anticipos)),2)
-#             dctTotal['monto_adeudado']=round(sum(map(lambda x:x['monto_adeudado'],lista_anticipos)),2)
-#             dctTotal['reglon']='total_detalle'
-
-
-#             lines.append(dctTotal)
-#         dctTotalGeneral={}
-#         dctTotalGeneral['numero_documento']='Total General'
-
-#         dctTotalGeneral['monto_anticipo']=round(sum(map(lambda x:x['monto_anticipo'],list(filter(lambda x: x['reglon']=='total_detalle', lines)))),2)
-#         dctTotalGeneral['monto_aplicado']=round(sum(map(lambda x:x['monto_aplicado'],list(filter(lambda x: x['reglon']=='total_detalle', lines)))),2)
-#         dctTotalGeneral['monto_adeudado']=round(sum(map(lambda x:x['monto_adeudado'],list(filter(lambda x: x['reglon']=='total_detalle', lines)))),2)
-#         dctTotalGeneral['reglon']='total_general'
-
-#         lines.append(dctTotalGeneral)
-#         lista_obj=[]
-#         for l in lines:
-#             obj_detalle=self.env['reporte.anticipo.detalle'].create(l)
-#             lista_obj.append(obj_detalle)
-
-#         return lista_obj
+        
+        attachment = self.env['ir.attachment'].create({
+            'datas': base64.b64encode(file_data.getvalue()),
+            'name': name,
+            'store_fname': name,
+            'type': 'binary',
+        })
+        url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        url += "/web/content/%s?download=true" %(attachment.id)
+        return{
+            "type": "ir.actions.act_url",
+            "url": url,
+            "target": "new",
+        }
 
 
+ 
+ 
+        
+        
+    def xslx_body(self, workbook, name):
+        bold = workbook.add_format({'bold':True,'border':1})
+        bold.set_center_across()
+        format_title = workbook.add_format({'bold':True,'border':1})
+        format_title.set_center_across()
+        currency_format = workbook.add_format({'num_format': '[$$-409]#,##0.00','border':1,'text_wrap': True })
+        currency_format.set_align('vcenter')
 
-# class ReporteEstadoCuentaDetalle(models.TransientModel):
-#     _name = "reporte.estado.de.cuenta.detalle"
+        
+        date_format = workbook.add_format({'num_format': 'dd/mm/yy', 'align': 'right','border':1,'text_wrap': True })
+        date_format.set_align('vcenter')
+        date_format_day = workbook.add_format({'align': 'right','border':1,'text_wrap': True })
+        date_format_day.set_align('vcenter')
+        date_format_title = workbook.add_format({'num_format': 'dd/mm/yy', 'align': 'left','text_wrap': True})
+        date_format_title.set_align('vcenter')
 
-#     numero_documento = fields.Char('Nro. Documento')
+        body = workbook.add_format({'align': 'center' , 'border':1,'text_wrap': True})
+        body.set_align('vcenter')
+        body_right = workbook.add_format({'align': 'right', 'border':1 })
+        body_left = workbook.add_format({'align': 'left','bold':True})
+        format_title2 = workbook.add_format({'align': 'center', 'bold':True,'border':1 })
+        sheet = workbook.add_worksheet(name)
+        sheet.merge_range('A1:B1', 'Fecha del informe:', bold)
+        sheet.merge_range('C1:I1', self.create_date, date_format_title)
 
-#     monto_anticipo = fields.Float('Monto Anticipo')
-#     monto_aplicado = fields.Float('Monto Aplicado')
-#     monto_adeudado = fields.Float('Monto Adeudado')
-#     observaciones = fields.Char('Observaciones')
-#     reglon = fields.Char('Reglon')
+
+        sheet.merge_range('A2:I2', 'Estado de Cuenta', bold)
+        sheet.merge_range('A3:C3', 'Monto:', bold)
+        sheet.merge_range('D3:E3', self.contrato_id.monto_financimiento, date_format_title)      
+
+        title_main=['No','Fecha','Fecha Pagada','Cuota Capital' ,'Cuota Administrativa', 'Iva Adm.', 'Factura','Seguro','Rastreo','Otros','Monto Pagado','Saldo','Estado de Pago']
+
+        ##Titulos
+        colspan=4
+        for col, head in enumerate(title_main):
+            sheet.set_column('{0}:{0}'.format(chr(col + ord('A'))), len(head) + 4)
+            sheet.write(5, col, head, bold)
+        
+        line = itertools.count(start=6)
+
+
+        for linea in self.contrato_id.estado_de_cuenta_ids:
+
+            current_line = next(line)
+            sheet.write(current_line, 0, linea.numero_cuota ,body)
+            sheet.write(current_line, 1, linea.fecha, date_format)
+            sheet.write(current_line, 2, linea.fecha_pagada  , date_format)
+            sheet.write(current_line, 3, linea.cuota_capital , currency_format)
+            sheet.write(current_line, 4, linea.cuota_adm ,currency_format)
+            sheet.write(current_line, 5, linea.iva_adm ,currency_format)
+            sheet.write(current_line, 6, linea.factura_id.name  , body)
+            sheet.write(current_line, 7, linea.seguro,currency_format)
+            sheet.write(current_line, 8, linea.rastreo,currency_format)
+            sheet.write(current_line, 9, linea.otro, currency_format)
+            sheet.write(current_line, 10, linea.monto_pagado, currency_format)
+            sheet.write(current_line, 11, linea.saldo, currency_format)
+            if linea.estado_pago=='pendiente':
+            	sheet.write(current_line, 12, 'Pendiente', body)
+            else:
+            	sheet.write(current_line, 12, 'Pagado', body)
+
 
