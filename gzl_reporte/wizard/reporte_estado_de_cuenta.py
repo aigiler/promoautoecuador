71bec8bd77e2cea7ec316a2b5884f0bc4e2139e3
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2022-01-12 17:28:14 -0500

    Update reporte_estado_de_cuenta.py

--- gzl_reporte/wizard/reporte_estado_de_cuenta.py
+++ gzl_reporte/wizard/reporte_estado_de_cuenta.py
@@ -81,9 +81,10 @@ class ReporteEstadoDeCuenta(models.TransientModel):
         sheet.merge_range('A8:M8', 'ESTADO DE CUENTA DE APORTES', format_subtitle)
         sheet.merge_range('A9:I9', 'Cliente: '+ self.contrato_id.cliente.name, format_datos)
         sheet.merge_range('A10:I10', 'Grupo: '+'['+ self.contrato_id.grupo.codigo+']'+ self.contrato_id.grupo.name, format_datos)
-        sheet.merge_range('A11:I11', 'Estado: '+ self.contrato_id.state+'('+self.contrato_id.fecha_adjudicado+')', format_datos)
+        #sheet.merge_range('A11:I11', 'Estado: '+ self.contrato_id.state+'('+self.contrato_id.fecha_adjudicado+')', format_datos)
+        sheet.merge_range('A11:I11', 'Estado: '+ self.contrato_id.state , format_datos)
         sheet.merge_range('A12:I12', 'Valor Inscripción: $'+ self.contrato_id.valor_inscripcion, format_datos)
-        #sheet.merge_range('D3:E3', self.contrato_id.monto_financiamiento, format_datos)      
+        #sheet.merge_range('D3:E3', self.contrato_id.monto_financiamiento, format_datos)
 
         title_main=['No','Fecha','Fecha Pagada','Cuota Capital' ,'Cuota Administrativa', 'Iva Adm.', 'Factura','Seguro','Rastreo','Otros','Monto Pagado','Saldo','Estado de Pago']
 
