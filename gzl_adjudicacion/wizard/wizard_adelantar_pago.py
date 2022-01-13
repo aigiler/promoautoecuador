7ec768513ab2e687a4f3c1c6c27412893cd5e362
Aldair Alarcon <aldoalar1992@gmail.com>
2022-01-12 23:40:14 -0500

    cambios en reporte estado de cuenta}

--- gzl_adjudicacion/wizard/wizard_adelantar_pago.py
+++ gzl_adjudicacion/wizard/wizard_adelantar_pago.py
@@ -12,7 +12,8 @@ class WizardAdelantarCuotas(models.TransientModel):
     _name = 'wizard.adelantar.cuotas'
     
     contrato_id = fields.Many2one('contrato')
-    numero_cuotas = fields.Integer( string='Nro. Cuotas')
+    numero_cuotas = fields.Integer( string='Nro. Cuotas',compute='calcular_numero_cuotas_a_cancelar',store=True)
+    monto_a_pagar = fields.Float( string='Monto a Pagar')
     payment_date = fields.Date(required=True, default=fields.Date.context_today)
     journal_id = fields.Many2one('account.journal', required=True, string='Diario', domain=[('type', 'in', ('bank', 'cash'))])
     payment_method_id = fields.Many2one('account.payment.method', string='Método de Pago', required=True)
@@ -31,17 +32,56 @@ class WizardAdelantarCuotas(models.TransientModel):
 
     
 
+    @api.depends('monto_a_pagar')
+    def calcular_numero_cuotas_a_cancelar(self):
+        for rec in self:
+            saldo=rec.contrato_id.tabla_amortizacion.mapped('saldo')
+            if len(saldo)>0:
+                valor_saldo=saldo[0]
+                rec.numero_cuotas=monto_a_pagar/saldo
+
+
+
     def validar_pago(self):
+
+        if self.numero_cuotas!=0 and monto_a_pagar>0:
+            raise ValidationError('El número de cuotas debe ser diferente de 0')
 
         tabla=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente')],order='fecha desc')
 
-        for detalle in tabla[:self.numero_cuotas]:
+
+
+
+
+        saldo=self.contrato_id.tabla_amortizacion.mapped('saldo')
+        valor_saldo=saldo[0]
+
+
+        lista_pagos=[]
+        for i  in range(0,self.numero_cuotas):
+            dct={
+            i:saldo
+            }
+            lista_pagos.append(dct)
+
+        diferencia=(saldo*self.numero_cuotas) - monto_a_pagar
+
+        if not (diferencia==0):
+            cuota_adicional=self.numero_cuotas+1
+            lista_pagos.append({cuota_adicional:abs(diferencia)})
+
+
+
+        contador=0
+
+        for detalle in tabla[len(lista_pagos)]:
             dct={
 
             'tabla_amortizacion_id':detalle.id,
             'payment_date':self.payment_date,
             'journal_id':self.journal_id.id,
             'payment_method_id':self.payment_method_id.id,
+            'amount':lista_pagos[contador]
 
             }
             pago=self.env['wizard.pago.cuota.amortizacion.contrato'].create(dct)
