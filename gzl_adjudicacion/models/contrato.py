5cf86614b524d8ea02a69c0484aaa78d613be1a9
Aldair Alarcon <aldoalar1992@gmail.com>
2022-01-13 01:34:19 -0500

    cambios en funcionalidad actualizar rubro

--- gzl_adjudicacion/models/contrato.py
+++ gzl_adjudicacion/models/contrato.py
@@ -553,7 +553,7 @@ class ContratoEstadoCuenta(models.Model):
 
 
 
-    @api.depends("seguro","rastreo","otros","pago_ids")
+    @api.depends("seguro","rastreo","otro","pago_ids")
     def calcular_monto_pagado(self):
 
         for l in self:
