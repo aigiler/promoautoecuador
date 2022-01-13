294c13a9be2e8c7505ce3bf0519f8ac9d008de12
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2022-01-13 05:19:31 -0500

    Update reporte_estado_de_cuenta.py

--- gzl_reporte/wizard/reporte_estado_de_cuenta.py
+++ gzl_reporte/wizard/reporte_estado_de_cuenta.py
@@ -67,11 +67,8 @@ class ReporteEstadoDeCuenta(models.TransientModel):
         date_format_title = workbook.add_format({'num_format': 'yyyy-mm-dd', 'align': 'justify','text_wrap': True})
         date_format_title.set_align('vcenter')
 
-        body = workbook.add_format({'align': 'center' , 'border':0,'text_wrap': True})
+        body = workbook.add_format({'align': 'left', 'indent':3 , 'border':0,'text_wrap': True})
         body.set_align('vcenter')
-        body_right = workbook.add_format({'align': 'right', 'border':1 })
-        body_left = workbook.add_format({'align': 'left','bold':True})
-        format_title2 = workbook.add_format({'align': 'center', 'bold':True,'border':1 })
         sheet = workbook.add_worksheet(name)
         sheet.insert_image('A1', 'resumen.jpg')
         sheet.merge_range('A3:I3', 'PROMOAUTO ECUADOR PROMOAUTOECUADOR S.A', format_title)
