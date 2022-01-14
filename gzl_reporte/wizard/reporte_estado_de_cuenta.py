818610b6d2530d24d7c2ac8d02fd7d90f10ac44b
amaciasSanchez <94863853+amaciasSanchez@users.noreply.github.com>
2022-01-14 17:15:57 -0500

    Update reporte_estado_de_cuenta.py

--- gzl_reporte/wizard/reporte_estado_de_cuenta.py
+++ gzl_reporte/wizard/reporte_estado_de_cuenta.py
@@ -80,9 +80,12 @@ class ReporteEstadoDeCuenta(models.TransientModel):
         body.set_align('vcenter')
         sheet = workbook.add_worksheet(name)
         # 
-        buf_image=io.BytesIO(base64.b64decode(self.env.company.image_1920))
-        sheet.insert_image('A2', "any_name.png",{'image_data': buf_image})
-
+        # buf_image=io.BytesIO(base64.b64decode(self.env.company.image_1920))
+        # sheet.insert_image('A2', "any_name.png",{'image_data': buf_image})
+        img = openpyxl.drawing.image.Image("../static/description/promoauto.png")
+        img.width = 72 * 7
+        img.height = 25 * 10
+        sheet.insert_image('A2', img)
         sheet.merge_range('A3:I3', self.env.company.name.upper(), format_title)
         sheet.merge_range('A5:I5', self.env.company.street.upper(), format_datos)
         # self.env.company.city.name
