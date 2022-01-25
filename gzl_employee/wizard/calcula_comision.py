cf34b4b395786a1bb1b9177000a20ef0fb8a906b
alisoncunalemaa <alisoncunalemaa@gmail.com>
2022-01-25 16:18:32 -0500

    calculo de comision nomina

--- gzl_employee/wizard/calcula_comision.py
+++ gzl_employee/wizard/calcula_comision.py
@@ -69,12 +69,17 @@ class CalculoComision(models.TransientModel):
                     #raise ValidationError(str(l.partner_id))
                     vals= {
                         'name':l.name,
-                        'planned_revenue':l.planned_revenue
+                        'planned_revenue':l.planned_revenue,
+                        'valor':str(l.planned_revenue),
+                        'crmlead':l.id,
+                        'company_id':l.company_id.id,
+                        'company_currency':l.company_currency.id
 
                     }
                     lines.append((0,0,vals))
-            #raise ValidationError(str(lines))
+            
             e.det = lines
+            #raise ValidationError(str(e.det))
                 #raise ValidationError(str(l.partner_id))
                 #hi = self.env['crm.lead'].filtered(lambda h:  h.partner_id == l.partner_id.id)
                 #raise ValidationError(str(hi))
@@ -87,10 +92,10 @@ class DetalleOportunidades(models.TransientModel):
     crmlead = fields.Many2one('crm.lead', string='Oportunidad')
     #oportunidad = fields.Char(string='Oportunidad')
     name = fields.Char('name')
-    planned_revenue = fields.Monetary('Expected Revenue', currency_field='company_currency', tracking=True)
+    planned_revenue = fields.Monetary('Monto del Plan', currency_field='company_currency', tracking=True)
     company_id = fields.Many2one('res.company', string='Company', index=True, default=lambda self: self.env.company.id)
     company_currency = fields.Many2one(string='Currency', related='company_id.currency_id', readonly=True, relation="res.currency")
-    valor = fields.Char('Monto')
+    #valor = fields.Char('Monto')
     sale_id = fields.Many2one('calcula.comision')
     #@api.onchange('crmlead')
     #def _onchange_picking_id(self):
