5f3acc47a49d01903213f81c46c8c164202dedb8
alisoncunalemaa <alisoncunalemaa@gmail.com>
2022-02-23 17:23:17 -0500

    proceso adendum

--- gzl_adjudicacion/wizard/wizard_contrato_adendum.py
+++ gzl_adjudicacion/wizard/wizard_contrato_adendum.py
@@ -114,11 +114,13 @@ class WizardContratoAdendum(models.Model):
                                                     })
         #crear el nuevo estado de cuenta 
         cuota_capital_nueva = (nuevoMontoReeestructura/int(intervalo_nuevo))
+        cuota_capital_nueva =round(cuota_capital_nueva, 2)
         #raise ValidationError(str(cuota_capital_nueva)+'-- cuota_capital_nueva')
         contb=0
         for i in range(cont, int(intervalo_nuevo+cont)):
             contb +=1
             cuota_capital = (nuevoMontoReeestructura/int(intervalo_nuevo))
+            cuota_capital =round(cuota_capital, 2)
             #raise ValidationError(str(cuota_capital)+'--'+str(round(nuevoMontoReeestructura/int(intervalo_nuevo))))
             cuota_adm = nuevoMontoReeestructura *tasa_administrativa / 100 / 12
             iva = cuota_adm * 0.12
@@ -203,30 +205,40 @@ class WizardContratoAdendum(models.Model):
 #################   ##############################################################################################3333
         #raise ValidationError(str(sum(self.contrato_id.tabla_amortizacion.mapped('cuota_capital')))+' contb '+ str(contb))
         monto_finan_contrato = sum(self.contrato_id.tabla_amortizacion.mapped('cuota_capital'))
+        monto_finan_contrato = round(monto_finan_contrato,2)
+        #raise ValidationError(str(monto_finan_contrato)+'-kk-'+str(self.contrato_id.monto_financiamiento)+'-kk-'+str(cuota_capital)+'---c---'+str(cuota_capital_nueva))
         if  monto_finan_contrato  > self.contrato_id.monto_financiamiento:
             valor_sobrante = monto_finan_contrato - self.contrato_id.monto_financiamiento 
+            valor_sobrante = round(valor_sobrante,2)
             parte_decimal, parte_entera = math.modf(valor_sobrante)
-            valor_a_restar= (valor_sobrante/parte_decimal)*0.1
+            if parte_decimal >=1:
+                valor_a_restar= (valor_sobrante/parte_decimal)*0.1
+            else:
+                valor_a_restar= (valor_sobrante/parte_decimal)*0.01
             
             obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente')] , order ='numero_cuota desc')
             #raise ValidationError(str(valor_sobrante)+'-kk-'+str(parte_decimal)+'----'+str(valor_a_restar))
             for c in obj_contrato:
-                #raise ValidationError(str(valor_sobrante)+'-kk-'+str(parte_decimal)+'----'+str(valor_a_restar)+'==='+str(monto_finan_contrato)+'==0='+str(c.cuota_capital))
-                valor_sobrante = valor_sobrante -valor_a_restar
+                #raise ValidationError(str(valor_sobrante)+'-parte_decimal: -'+str(parte_decimal)+'---valor_a_restar -'+str(valor_a_restar)+'==='+str(monto_finan_contrato)+'==cuota_capital ='+str(c.cuota_capital))
                 
-                if valor_sobrante != 0.00 or valor_sobrante != 0:
+                #raise ValidationError(str(valor_sobrante)+'-parte_decimal: -'+str(parte_decimal)+'---valor_a_restar -'+str(valor_a_restar)+'==='+str(monto_finan_contrato)+'==cuota_capital ='+str(c.cuota_capital))
+                if valor_sobrante != 0.00 or valor_sobrante != 0 or valor_sobrante != 0.0:
                      c.update({
                         'cuota_capital': c.cuota_capital - valor_a_restar,
                         'contrato_id':self.contrato_id.id,
                     })
+                valor_sobrante = valor_sobrante -valor_a_restar
                 
         if  monto_finan_contrato  < self.contrato_id.monto_financiamiento:
             valor_sobrante = self.contrato_id.monto_financiamiento  - monto_finan_contrato 
             
             parte_decimal, parte_entera = math.modf(valor_sobrante)
             #raise ValidationError(str(valor_sobrante)+'--'+str(parte_decimal)+'----'+str(monto_finan_contrato))
-            valor_a_restar= (valor_sobrante/parte_decimal)*0.1
-            
+            #valor_a_restar= (valor_sobrante/parte_decimal)*0.1
+            if parte_decimal >=1:
+                valor_a_restar= (valor_sobrante/parte_decimal)*0.1
+            else:
+                valor_a_restar= (valor_sobrante/parte_decimal)*0.01            
             obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente')] , order ='numero_cuota desc')
             
             for c in obj_contrato:
