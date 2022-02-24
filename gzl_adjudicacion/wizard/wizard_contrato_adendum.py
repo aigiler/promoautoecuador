312563505ba009e7ddf46d0d3ebb711cb18b52d7
alisoncunalemaa <alisoncunalemaa@gmail.com>
2022-02-24 09:41:11 -0500

    proceso adendum

--- gzl_adjudicacion/wizard/wizard_contrato_adendum.py
+++ gzl_adjudicacion/wizard/wizard_contrato_adendum.py
@@ -121,7 +121,6 @@ class WizardContratoAdendum(models.Model):
             contb +=1
             cuota_capital = (nuevoMontoReeestructura/int(intervalo_nuevo))
             cuota_capital =round(cuota_capital, 2)
-            #raise ValidationError(str(cuota_capital)+'--'+str(round(nuevoMontoReeestructura/int(intervalo_nuevo))))
             cuota_adm = nuevoMontoReeestructura *tasa_administrativa / 100 / 12
             iva = cuota_adm * 0.12
             #monto_finan_contrato+= cuota_capital
@@ -167,46 +166,20 @@ class WizardContratoAdendum(models.Model):
                                                         'cuota_adm':cta_ant['cuota_adm'],
                                                         'iva_adm':cta_ant['iva_adm'],
                                                         'saldo':cta_ant['saldo'],
-                                                        #'contrato_id':cta_ant['contrato_id'],
                                                         'estado_pago':cta_ant['estado_pago'], 
                                                         'cuotaAdelantada':cta_ant['cuotaAdelantada'] ,  
                                                         'seguro':cta_ant['seguro'],  
                                                         'fecha_pagada':cta_ant['fecha_pagada'] ,  
                                                         'rastreo':cta_ant['rastreo'],  
                                                         'factura_id':cta_ant['factura_id'] ,#or None,          
-                                                        'contrato_id':int(obj_estado_cuenta_cabecera.id),           
-                                                        #'currency_id':cta_ant['currency_id'],   
+                                                        'contrato_id':int(obj_estado_cuenta_cabecera.id), 
                                                             })
-            
-
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             
             
 #################   ##############################################################################################3333
         #raise ValidationError(str(sum(self.contrato_id.tabla_amortizacion.mapped('cuota_capital')))+' contb '+ str(contb))
         monto_finan_contrato = sum(self.contrato_id.tabla_amortizacion.mapped('cuota_capital'))
         monto_finan_contrato = round(monto_finan_contrato,2)
-        #raise ValidationError(str(monto_finan_contrato)+'-kk-'+str(self.contrato_id.monto_financiamiento)+'-kk-'+str(cuota_capital)+'---c---'+str(cuota_capital_nueva))
         if  monto_finan_contrato  > self.contrato_id.monto_financiamiento:
             valor_sobrante = monto_finan_contrato - self.contrato_id.monto_financiamiento 
             valor_sobrante = round(valor_sobrante,2)
@@ -218,37 +191,33 @@ class WizardContratoAdendum(models.Model):
             
             obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente')] , order ='numero_cuota desc')
             #raise ValidationError(str(valor_sobrante)+'-kk-'+str(parte_decimal)+'----'+str(valor_a_restar))
+            
             for c in obj_contrato:
                 #raise ValidationError(str(valor_sobrante)+'-parte_decimal: -'+str(parte_decimal)+'---valor_a_restar -'+str(valor_a_restar)+'==='+str(monto_finan_contrato)+'==cuota_capital ='+str(c.cuota_capital))
-                
-                #raise ValidationError(str(valor_sobrante)+'-parte_decimal: -'+str(parte_decimal)+'---valor_a_restar -'+str(valor_a_restar)+'==='+str(monto_finan_contrato)+'==cuota_capital ='+str(c.cuota_capital))
                 if valor_sobrante != 0.00 or valor_sobrante != 0 or valor_sobrante != 0.0:
-                     c.update({
+                    
+                    c.update({
                         'cuota_capital': c.cuota_capital - valor_a_restar,
                         'contrato_id':self.contrato_id.id,
-                    })
-                valor_sobrante = valor_sobrante -valor_a_restar
-                
+                     })
+                    valor_sobrante = valor_sobrante -valor_a_restar  
         if  monto_finan_contrato  < self.contrato_id.monto_financiamiento:
             valor_sobrante = self.contrato_id.monto_financiamiento  - monto_finan_contrato 
             
             parte_decimal, parte_entera = math.modf(valor_sobrante)
-            #raise ValidationError(str(valor_sobrante)+'--'+str(parte_decimal)+'----'+str(monto_finan_contrato))
-            #valor_a_restar= (valor_sobrante/parte_decimal)*0.1
-            if parte_decimal >=1:
-                valor_a_restar= (valor_sobrante/parte_decimal)*0.1
-            else:
-                valor_a_restar= (valor_sobrante/parte_decimal)*0.01            
+            valor_a_restar= (valor_sobrante/parte_decimal)*0.1
+            
             obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente')] , order ='numero_cuota desc')
             
             for c in obj_contrato:
-                valor_sobrante = valor_sobrante -valor_a_restar
-                if valor_sobrante != 0.00 or valor_sobrante != 0:
+                
+                if valor_sobrante != 0.00 or valor_sobrante != 0 or valor_sobrante != 0.0:
                     #raise ValidationError(str(valor_sobrante)+'--'+str(parte_decimal)+'----'+str(valor_a_restar))
                     c.update({
                         'cuota_capital': c.cuota_capital + valor_a_restar,
                         'contrato_id':self.contrato_id.id,
-                    })   
+                    })  
+                    valor_sobrante = valor_sobrante -valor_a_restar
         ##si esta ejecutado se ocultara el boton de validar                  
         self.ejecutado =True
         #asignar nuevos valores 
