cda6080d839e7b6c1ba572433c8e1620de9f96a9
alisoncunalemaa <alisoncunalemaa@gmail.com>
2022-03-03 16:30:05 -0500

    envio correo nomina

--- gzl_reporte/wizard/nomina_mesual_correo.py
+++ gzl_reporte/wizard/nomina_mesual_correo.py
@@ -30,8 +30,8 @@ class Nomina_mensual(models.TransientModel):
     #_inherit ="hr.employee"
     def _get_available_contracts_domain(self):
         payslip = self.env['hr.payslip'].search([('date_from','>=',self.fecha_inicio),('date_to','<=',self.fecha_fin)])
-
-        return [('contract_ids.state', 'in', ('open', 'close')), ('company_id', '=', self.env.company.id)],('id', 'in', payslip.ids)
+        #raise ValidationError(str(list(payslip.employee_id.ids))+' payslip')
+        return [('contract_ids.state', 'in', ('open', 'close')), ('company_id', '=', self.env.company.id),('id', 'in', list(payslip.employee_id.ids))]
 
     def _get_employees(self):
         # YTI check dates too
@@ -46,12 +46,31 @@ class Nomina_mensual(models.TransientModel):
     work_email = fields.Char('Work Email')
     url_doc = fields.Char('Url doc')
 
+
     def actualizar_empleados_payroll(self):   
-        self.employee_ids_correo=self.env['hr.employee'].search(self._get_available_contracts_domain())
+        self.employee_ids_correo=self._get_employees()
+        #self.ensure_one()
+        #self.name = "New name"
+        #return {
+        #    "type": "ir.actions.do_nothing",
+        #}
+        self.ensure_one()
+        self.name = "New name"
+        return {
+            'context': self.env.context,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'correo.nomina.mensual',
+            'res_id': self.id,
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
+        #raise ValidationError(str(self.employee_ids_correo)+' result')
     def send_mail_payrol(self):
         lis=[]
         #result = self.env['hr.payslip']
-        #raise ValidationError(str(result)+' result')
+        raise ValidationError(str(self.employee_ids_correo)+' result')
         for l in self.employee_ids_correo:
             #result = self.env['hr.payslip'].search([('employee_id', '=', l.id)])
             payslip = self.env['hr.payslip'].search([('employee_id','=',l.id),('date_from','>=',self.fecha_inicio),('date_to','<=',self.fecha_fin)])
