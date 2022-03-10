e878c471d073fa32a5f7218eb3aec1c831306338
alisoncunalemaa <alisoncunalemaa@gmail.com>
2022-03-10 13:08:21 -0500

    devolucion

--- gzl_adjudicacion/models/devolucion_monto_vehiculo.py
+++ gzl_adjudicacion/models/devolucion_monto_vehiculo.py
@@ -14,3 +14,14 @@ class DevolucionMonto(models.Model):
     monto = fields.Float(string='Monto')
     contrato_id = fields.Many2one('contrato')
     fsolicitud  = fields.Date(string='Fecha de Solicitud')
+    state = fields.Selection(selection=[
+        ('borrador', 'Borrador'),
+        ('postventa', 'Analisis Postventa'),
+        ('legal', 'Analisis Legal'),
+        ('adjudicaciones', 'Analisis Adjudicacion'),
+        ('verifvalores', 'Verificacion de Valores'),
+        ('aprobgerencia', 'Aprobacion Gerencia'),
+        ('salidadinero', 'Salida Dinero'),
+        ('notificacion', 'Notificacion Cliente'),
+        ('liquidacion', 'Liquidacion de vendedor'),
+    ], string='Estado', default='borrador', track_visibility='onchange')
