7652c817c870e5a7931827a013be63dcd3e69f1d
DESKTOP-LUOG93R\Aldair Alarcon <Aldair Alarcon>
2022-04-18 14:40:03 -0500

    cambios en secuencia de contrato para tomar secuencia del grupo

--- gzl_adjudicacion/models/contrato.py
+++ gzl_adjudicacion/models/contrato.py
@@ -326,10 +326,11 @@ class Contrato(models.Model):
     @api.model
     def create(self, vals):
         
-        secuencia_codigo= self.env['grupo.adjudicado'].browse(vals.get('grupo',False)).id
 
-        vals['secuencia'] = self.env['ir.sequence'].next_by_code(secuencia_codigo)
 
+        obj_secuencia= self.grupo.secuencia_id
+
+        vals['secuencia'] = obj_secuencia.next_by_code(obj_secuencia.code)
         dia_corte =  self.env['ir.config_parameter'].sudo().get_param('gzl_adjudicacion.dia_corte')
         tasa_administrativa =  self.env['ir.config_parameter'].sudo().get_param('gzl_adjudicacion.tasa_administrativa')
 
