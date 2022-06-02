8ebae3bdf47a6744eb164990507cb35b0c7bacb5
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-02 14:36:06 -0500

    campos contrato y estado de cuenta en facturas

--- l10n_ec_check_printing/models/account_payment.py
+++ l10n_ec_check_printing/models/account_payment.py
@@ -456,7 +456,19 @@ class AccountPayment(models.Model):
             #for l in rec.payment_line_ids:
                 #if l.amount>l.actual_amount:
                     #raise ValidationError("El monto a pagar no puede ser al monto adeudado en la factura {0}".format(l.invoice_id.l10n_latam_document_number))
-
+            lista_invoice=[]
+            if rec.tipo_valor:
+                for pago in rec.payment_line_ids:
+                    if pago.pagar:
+                        lista_invoice.append(pago.invoice_id.id)
+                        movimientos_occ=self.env['account.move'].search([('journal_id','=',21),('ref','=',pago.invoice_id.name)])
+                        if movimientos_occ:
+                            for mov in movimientos_occ:
+                                lista_invoice.append(mov.id)
+                                raise ValidationError("se encuentra este movimiento{0}".format(mov.id))     
+                        else:
+                            raise ValidationError("NO se encuentra este movimiento{0}".format(pago.invoice_id.name))        
+                rec.update({'invoice_ids': [(6, 0, lista_invoice)]})
 
             
 
@@ -504,21 +516,7 @@ class AccountPayment(models.Model):
                 self.invoice_ids = invoice_id
             account_check = rec.env['account.cheque']
             
-            lista_invoice=[]
-            if rec.tipo_valor:
-                for pago in rec.payment_line_ids:
-                    if pago.pagar:
-                        
 
-                        lista_invoice.append(pago.invoice_id.id)
-                        movimientos_occ=self.env['account.move'].search([('journal_id','=',21),('ref','=',pago.invoice_id.name)])
-                        if movimientos_occ:
-                            for mov in movimientos_occ:
-                                lista_invoice.append(mov.id)
-                                raise ValidationError("se encuentra este movimiento{0}".format(mov.id))     
-                        else:
-                            raise ValidationError("NO se encuentra este movimiento{0}".format(pago.invoice_id.name))        
-                rec.update({'invoice_ids': [(6, 0, lista_invoice)]})
  
             if rec.payment_method_id.code in ['check_printing','batch_payment'] and not rec.payment_type == 'transfer':
                 #types = 'outgoing'
