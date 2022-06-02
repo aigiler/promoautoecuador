c63b9409c9b07cef925e1e2aec1fd6459eb83171
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-02 12:26:51 -0500

    campos contrato y estado de cuenta en facturas

--- l10n_ec_check_printing/models/account_payment.py
+++ l10n_ec_check_printing/models/account_payment.py
@@ -461,9 +461,10 @@ class AccountPayment(models.Model):
             lista_invoice=[]
 
             for pago in rec.payment_line_ids:
-                movimientos_occ=self.env['account.move'].search([('journal_id','=',21),('ref','=',pago.invoice_id.name)])[0]
+                movimientos_occ=self.env['account.move'].search([('journal_id','=',21),('ref','=',pago.invoice_id.name)])
                 if movimientos_occ:
-                    lista_invoice.append(movimientos_occ.id)
+                    for mov in movimientos_occ:
+                        lista_invoice.append(movimientos_occ.id)
                 if pago.pagar:
                     lista_invoice.append(pago.invoice_id.id)
             rec.update({'invoice_ids': [(6, 0, lista_invoice)]})
