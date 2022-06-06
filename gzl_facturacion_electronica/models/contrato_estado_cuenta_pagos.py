8715e63736b679ddac0e5da017d077c3dd4baaeb
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-06 13:04:04 -0500

    Apuntes contables en pagos antes de publicar

--- gzl_facturacion_electronica/models/contrato_estado_cuenta_pagos.py
+++ gzl_facturacion_electronica/models/contrato_estado_cuenta_pagos.py
@@ -58,6 +58,7 @@ class ContratoEstadoCuentaPagos(models.Model):
         for l in self:
             l.monto_pagar=l.cuota_capital_pagar+l.seguro_pagar+l.rastreo_pagar+l.otro_pagar
             l.payment_pagos_id._saldo_pagar()
+            l.crear_asientos()
 
 
     # @api.onchange('cuota_capital_pagar','seguro_pagar','rastreo_pagar','otro_pagar')
