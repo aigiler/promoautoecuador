68658ef67305e533adf7e6de3faf1b5019ef0bd6
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-06 23:24:51 -0500

    Apuntes contables en pagos antes de publicar

--- account/models/account_move.py
+++ account/models/account_move.py
@@ -3823,12 +3823,12 @@ class AccountMoveLine(models.Model):
 
         
         # List unpaid invoices
-        #not_paid_invoices = self.mapped('move_id').filtered(
-        #    lambda m: m.is_invoice(include_receipts=True) and m.invoice_payment_state not in ('paid', 'in_payment')
-        #)
         not_paid_invoices = self.mapped('move_id').filtered(
-            lambda m: m.invoice_payment_state not in ('paid', 'in_payment')
+            lambda m: m.is_invoice(include_receipts=True) and m.invoice_payment_state not in ('paid', 'in_payment')
         )
+        #not_paid_invoices = self.mapped('move_id').filtered(
+        #    lambda m: m.invoice_payment_state not in ('paid', 'in_payment')
+        #)
         #raise ValidationError("**********************************{0}".format(not_paid_invoices))
 
         reconciled_lines = self.filtered(lambda aml: float_is_zero(aml.balance, precision_rounding=aml.move_id.company_id.currency_id.rounding) and aml.reconciled)
