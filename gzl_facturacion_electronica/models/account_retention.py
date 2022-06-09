9624087edbd4229182f6a03ffedafaab791385d5
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-09 14:26:39 -0500

    Reporte de Prospecto

--- gzl_facturacion_electronica/models/account_retention.py
+++ gzl_facturacion_electronica/models/account_retention.py
@@ -384,13 +384,13 @@ class AccountRetention(models.Model):
                 #if line.tax_repartition_line_id.account_id:
                 #    raise ValidationError("Verifique que en la sección Repartición de Facturas del Menu Impestos se encuentre configurada la cuenta.")
 
-                lines.append((0, 0, {
-                    'partner_id': ret.partner_id.id,
-                    'account_id': line.tax_repartition_line_id.account_id.id,
-                    'name': ret.name,
-                    'debit': abs(line.amount),
-                    'credit': 0.00
-                }))
+            lines.append((0, 0, {
+                'partner_id': ret.partner_id.id,
+                'account_id': line.tax_repartition_line_id.account_id.id,
+                'name': ret.name,
+                'debit': abs(line.amount),
+                'credit': 0.00
+            }))
 
                 total_counter += abs(line.amount)
             rec_account = inv.partner_id.property_account_receivable_id.id
