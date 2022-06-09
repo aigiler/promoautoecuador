223b2973d57f753a6e55357ff4e03af7732404d2
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-09 12:28:56 -0500

    Reporte de Prospecto

--- account/models/account_payment.py
+++ account/models/account_payment.py
@@ -938,7 +938,7 @@ class account_payment(models.Model):
                                                         if lista:
                                                             for z in lista:
                                                                 if z[2]['debit_move_id']== x.id:
-                                                                    z[2]['amount']=monto_cruzado
+                                                                    z[2]['amount']+=cuota_id.saldo_cuota_capital
                                                         else:
                                                             tupla=(0, 0, {
                                                                     'debit_move_id': x.id,
