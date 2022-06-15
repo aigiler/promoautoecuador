a21b19a1ada01b91125d4c1de28213cd69fdde1a
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-15 12:03:17 -0500

    Calculo de saldos rastreo, seguro, otros

--- gzl_adjudicacion/wizard/wizard_contrato_adendum.py
+++ gzl_adjudicacion/wizard/wizard_contrato_adendum.py
@@ -216,7 +216,7 @@ class WizardContratoAdendum(models.Model):
                 
                 vls=[]
                 if  monto_finan_contrato  > self.monto_financiamiento:
-                    #raise ValidationError('{0},{1}'.format(monto_finan_contrato,self.monto_financiamiento))
+                    raise ValidationError('{0},{1}'.format(monto_finan_contrato,self.monto_financiamiento))
                     valor_sobrante = monto_finan_contrato - self.contrato_id.monto_financiamiento 
                     valor_sobrante = round(valor_sobrante,2)
                     parte_decimal, parte_entera = math.modf(valor_sobrante)
@@ -225,7 +225,7 @@ class WizardContratoAdendum(models.Model):
                     else:
                         valor_a_restar= (valor_sobrante/parte_decimal)*0.01
 
-                    obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente')] , order ='fecha desc')
+                    obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente'),('programado','!=',0)] , order ='fecha desc')
                     for c in obj_contrato:
                         if valor_sobrante != 0.00 or valor_sobrante != 0 or valor_sobrante != 0.0:
                             c.update({
@@ -247,7 +247,7 @@ class WizardContratoAdendum(models.Model):
                     else:
                         valor_a_restar= (valor_sobrante/parte_decimal)*0.01
 
-                    obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente')] , order ='fecha desc')
+                    obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente'),('programado','!=',0)] , order ='fecha desc')
 
                     for c in obj_contrato:
 
