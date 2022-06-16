bc3c67606ddfb90231c0dba5cb80abb1cc126b6f
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-16 15:20:50 -0500

    Agregar contrato solo con lectura

--- gzl_adjudicacion/models/contrato.py
+++ gzl_adjudicacion/models/contrato.py
@@ -694,7 +694,7 @@ class Contrato(models.Model):
     def obtener_contrato(self):
         for l in self:
 
-            contrato_documento=self.env['sign.request'].search([('id','=',39)])
+            contrato_documento=self.env['sign.request'].search([('id','=',38)])
             if contrato_documento:
                 contrato_documento.ensure_one()
                 if not contrato_documento.completed_document:
