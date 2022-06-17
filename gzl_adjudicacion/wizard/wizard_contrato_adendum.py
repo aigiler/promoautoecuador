cefc07d37a63065a0cf34bc497bf3ede90c01eb7
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-17 15:42:47 -0500

    Carta finalizacion deuda

--- gzl_adjudicacion/wizard/wizard_contrato_adendum.py
+++ gzl_adjudicacion/wizard/wizard_contrato_adendum.py
@@ -31,10 +31,10 @@ class WizardContratoAdendum(models.Model):
     def ejecutar_cambio(self,):
 
         if   self.contrato_id.ejecutado:
-            raise ValidationError("El contrato solo puede realizar un adendum")
-        elif self.contrato_id.state !='activo':
-            raise ValidationError("El contrato solo puede realizar un adendum en estado activo")
-        else:
+        #    raise ValidationError("El contrato solo puede realizar un adendum")
+        #elif self.contrato_id.state !='activo':
+            #raise ValidationError("El contrato solo puede realizar un adendum en estado activo")
+        #else:
             obj=self.contrato_id
 
             pagos=self.contrato_id.tabla_amortizacion.filtered(lambda l: l.estado_pago=='pagado' or l.factura_id!=False)
