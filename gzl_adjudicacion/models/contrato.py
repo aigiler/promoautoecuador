a41ad2e1edd4c4d3908e36eb88bb2ce8d8f56571
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-20 12:58:45 -0500

    Modificar Contrato

--- gzl_adjudicacion/models/contrato.py
+++ gzl_adjudicacion/models/contrato.py
@@ -325,8 +325,11 @@ class Contrato(models.Model):
         monto_finan_contrato = sum(self.tabla_amortizacion.mapped('cuota_capital'))
         monto_finan_contrato = round(monto_finan_contrato,2)
         #raise ValidationError(str(monto_finan_contrato))
-        if  monto_finan_contrato  > self.monto_financiamiento:
-            valor_sobrante = monto_finan_contrato - self.monto_financiamiento 
+        monto_financiamiento_contrato=self.monto_financiamiento
+        if self.tiene_cuota:
+            monto_financiamiento_contrato=self.monto_financiamiento-self.monto_programado
+        if  monto_finan_contrato  > monto_financiamiento_contrato:
+            valor_sobrante = monto_finan_contrato - monto_financiamiento_contrato 
             valor_sobrante = round(valor_sobrante,2)
             parte_decimal, parte_entera = math.modf(valor_sobrante)
             if parte_decimal==0:
@@ -350,8 +353,8 @@ class Contrato(models.Model):
                         valor_sobrante = round(valor_sobrante,2)
                             
                             
-        if  monto_finan_contrato  < self.monto_financiamiento:
-            valor_sobrante = self.monto_financiamiento  - monto_finan_contrato 
+        if  monto_finan_contrato  < monto_financiamiento_contrato:
+            valor_sobrante = monto_financiamiento_contrato  - monto_finan_contrato 
             valor_sobrante = round(valor_sobrante,2)
             parte_decimal, parte_entera = math.modf(valor_sobrante)
             if parte_decimal==0:
