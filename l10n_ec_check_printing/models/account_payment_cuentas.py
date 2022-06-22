1c0e0166cdcd60b12d8620360a4bb301d00993d0
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-22 13:42:53 -0500

    Pagos

--- l10n_ec_check_printing/models/account_payment_cuentas.py
+++ l10n_ec_check_printing/models/account_payment_cuentas.py
@@ -23,7 +23,7 @@ class PaymentLineAccount(models.Model):
     @api.onchange('credit')
     def validar_anticipo(self):
         for l in self:
-            if round(l.payment_id.credito,2)==round(l.credit,2) and l.credito_contrato:
+            if round(l.payment_id.credito,2)==round(l.credit,2) and l.payment_id.credito_contrato:
                l.aplicar_anticipo=True 
 
 
