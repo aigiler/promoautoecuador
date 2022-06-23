5f7cb6eb0b11951a74302e1a777f359dee494ca9
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-23 14:08:31 -0500

    Pagos

--- gzl_reporte/wizard/crear_documento_contrato_reserva.py
+++ gzl_reporte/wizard/crear_documento_contrato_reserva.py
@@ -47,6 +47,7 @@ def crear_documento_reserva(ruta,detalle,lista_estado_cuenta):
         regex1 = re.compile(campo['identificar_docx'])
 
       #  Reemplaza los valores de identificadores de la plantilla con los del json
+        raise ValidationError('{0}'.format(regex1))
         docx_replace_regex_ram(doc,regex1,campo['valor'] or "" )
         docx_replace_regex_header_ram(doc.sections[0].header,regex1,campo['valor'] or "")
 
