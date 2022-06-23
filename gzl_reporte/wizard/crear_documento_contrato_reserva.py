b897e927b429c3f709b074becad6f46058159a0b
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-23 14:24:19 -0500

    Pagos

--- gzl_reporte/wizard/crear_documento_contrato_reserva.py
+++ gzl_reporte/wizard/crear_documento_contrato_reserva.py
@@ -98,7 +98,7 @@ def docx_replace_regex_header_ram(doc_obj, regex , replace):
 def docx_replace_regex_ram(doc_obj, regex , replace):
 
     for p in doc_obj.paragraphs:
-        raise ValidationError("{0},wwwwwwwwwwwwwwww".format(p.text))
+        raise ValidationError("{0},wwwwwwwwwwwwwwww".format(doc_obj.paragraphs[2].text))
         if regex.search(p.text):
 
             inline = p.runs
