bb03e3ca46f6a9705eab960f44d3ebd648ab0b38
Daylin Murga <melinarosado1996daylin@gmail.com>
2022-06-24 10:55:59 -0500

    Pagos

--- gzl_reporte/wizard/crear_documento_contrato_reserva.py
+++ gzl_reporte/wizard/crear_documento_contrato_reserva.py
@@ -111,11 +111,8 @@ def docx_replace_regex_ram(doc_obj, regex , replace):
 
     palabras=[]
     for p in doc_obj.paragraphs:
-        if regex in p.text:
-            inline = p.runs
-            i=0
-            palabras=inline.split(" ")
-    raise ValidationError('{0}'.format(palabras))
+
+        raise ValidationError('{0}'.format(p.text))
                 #if regex in run.text:
             # Loop added to work with runs (strings with same style)
             #for i in range(len(inline)):
