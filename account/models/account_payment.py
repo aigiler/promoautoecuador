ef392858a4a146c41580c2b7d71f8d5c20bff02a
daylin2022 <daylin.murga@gizlocorp.com>
2022-06-28 13:37:34 -0500

    Pagos de cuotas parcial

--- account/models/account_payment.py
+++ account/models/account_payment.py
@@ -1075,8 +1075,7 @@ class account_payment(models.Model):
                         (moves[0] + rec.invoice_ids).line_ids \
                             .filtered(lambda line: not line.reconciled and line.account_id == rec.destination_account_id and not (line.account_id == line.payment_id.writeoff_account_id and line.name == line.payment_id.writeoff_label))\
                             .reconcile()
-
-
+                            
             elif rec.payment_type == 'transfer':
                 # ==== 'transfer' ====
                 moves.mapped('line_ids')\
