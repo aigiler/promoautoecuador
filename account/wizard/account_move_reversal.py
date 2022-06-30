7f9f06e50a3880e539e4d8265b755ba35478c783
daylin2022 <daylin.murga@gizlocorp.com>
2022-06-30 16:30:58 -0500

    modificar cuotas

--- account/wizard/account_move_reversal.py
+++ account/wizard/account_move_reversal.py
@@ -131,7 +131,7 @@ class AccountMoveReversal(models.TransientModel):
             movimientos_cuota=self.env['account.move'].search([('ref','=',self.move_id.name),('journal_id','in',lista_diarios)])
             for mov in movimientos_cuota:
                 movimiento_reverso_id=self.env['account.move.reversal'].create({'move_id':mov.id,'date':fields.Date.context_today(self),
-                                        'journal_id':mov.journal_id.id})
+                                        'journal_id':mov.journal_id.id,'move_type':'entry'})
                                 #raise ValidationError("{0}".format(action_rubros))
                 movimiento_reverso_id.reverse_moves()
         return action
