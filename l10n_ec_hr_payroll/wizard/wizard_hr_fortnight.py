a38f2a2f905cf142e8fec51aa2260ef076078279
daylin2022 <melinarosado1996daylin@gmail.com>
2022-07-06 10:32:12 -0500

    modificar cuotas

--- l10n_ec_hr_payroll/wizard/wizard_hr_fortnight.py
+++ l10n_ec_hr_payroll/wizard/wizard_hr_fortnight.py
@@ -54,7 +54,7 @@ class WizardPayFortnight(models.TransientModel):
         if self.line_ids:
             line_ids = self.line_ids.split(',')
             for values in line_ids:
-                data.append(int(values))
+                data.append(int(values.id))
         input_ids = self.env['hr.input'].browse(data)
 
         for e in employee_ids:
