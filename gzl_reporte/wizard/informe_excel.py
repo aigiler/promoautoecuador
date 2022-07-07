970d85cdb0bc978a4e40a734a64945f7ac1a8d92
daylin2022 <melinarosado1996daylin@gmail.com>
2022-07-07 09:32:05 -0500

    modificar cuotas

--- gzl_reporte/wizard/informe_excel.py
+++ gzl_reporte/wizard/informe_excel.py
@@ -104,7 +104,7 @@ def informe_credito_cobranza(ruta,lista,lista_patrimonio, lista_paginas, lista_p
     if garante:
         listaSheet3 = list(filter(lambda x: (x['hoja']==5), lista)) 
     for campo in listaSheet3:
-        if campo['fila']==25 and campo['comumna']==5:
+        if campo['fila']==25 and campo['columna']==5:
             if campo['valor']!='':
                 campo['valor']=campo['valor']/100
         cell = sheet.cell(row=campo['fila'], column=campo['columna'])
