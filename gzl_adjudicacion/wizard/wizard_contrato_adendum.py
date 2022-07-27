846ef6c4e225b94a59b8068945d8bffb6ab26d45
daylin2022 <melinarosado1996daylin@gmail.com>
2022-07-27 09:52:37 -0500

    Coreecion

--- gzl_adjudicacion/wizard/wizard_contrato_adendum.py
+++ gzl_adjudicacion/wizard/wizard_contrato_adendum.py
@@ -19,7 +19,9 @@ class WizardContratoAdendum(models.Model):
     monto_financiamiento = fields.Monetary(
         string='Monto Financiamiento', currency_field='currency_id', track_visibility='onchange')
     plazo_meses = fields.Many2one('numero.meses',default=lambda self: self.env.ref('gzl_adjudicacion.{0}'.format('numero_meses60')).id ,track_visibility='onchange' )
-
+    cuota_adm = fields.Monetary(
+        string='Cuota Administrativa',store=True, compute='calcular_cuota', currency_field='currency_id', track_visibility='onchange')
+    
     currency_id = fields.Many2one(
         'res.currency', readonly=True, default=lambda self: self.env.company.currency_id)
     ##valores anteriores
@@ -28,6 +30,17 @@ class WizardContratoAdendum(models.Model):
     plazo_meses_anterior = fields.Many2one('numero.meses',default=lambda self: self.env.ref('gzl_adjudicacion.{0}'.format('numero_meses60')).id ,track_visibility='onchange' )
     cuota_anterior = fields.Monetary(currency_field='currency_id', track_visibility='onchange')
     observacion = fields.Char(string='Observacion')
+
+    @api.depends('monto_financiamiento')
+    def calcular_cuota(self):
+        for l in self:
+            cuotaAdministrativa=0
+            if l.monto_financiamiento:
+                cuotaAdministrativa= (l.monto_financiamiento*((l.contrato_id.tasa_administrativa/100)/12))*l.plazo_meses
+                
+                
+            l.cuota_adm = cuotaAdministrativa
+
     def ejecutar_cambio(self,):
 
         if   self.contrato_id.ejecutado:
@@ -40,14 +53,18 @@ class WizardContratoAdendum(models.Model):
 
             pagos=self.contrato_id.tabla_amortizacion.filtered(lambda l: l.estado_pago=='pagado')
             cuotas_pgadas=sum(pagos.mapped("cuota_capital"))
+            adm_pgadas=sum(pagos.mapped("cuota_adm"))
             pagos_pendiente=self.contrato_id.tabla_amortizacion.filtered(lambda l: l.estado_pago!='pagado' and l.factura_id)
             cuotas_pendientes_pago=sum(pagos_pendiente.mapped("cuota_capital"))
+            adm_pendientes_pago=sum(pagos_pendiente.mapped("cuota_adm"))
             abonos=self.contrato_id.tabla_amortizacion.filtered(lambda l: l.estado_pago!='pagado' and l.monto_pagado>0 and not l.factura_id)
             cuotas_pendientes_abono=sum(abonos.mapped("cuota_capital"))
+            adm_pendientes_abono=sum(abonos.mapped("cuota_adm"))
             pago_capital=cuotas_pgadas+cuotas_pendientes_pago+cuotas_pendientes_abono
+            pago_adm=adm_pgadas+adm_pendientes_pago+adm_pendientes_abono
 
             nuevoMontoReeestructura=self.monto_financiamiento-pago_capital
-
+            nuevoCuotaAdm=self.cuota_adm-pago_adm
 
             cuotasPagadas=self.contrato_id.tabla_amortizacion.filtered(lambda l: l.estado_pago=='pagado' and l.cuotaAdelantada==False)
             numcuotas_congeladas=self.contrato_id.tabla_amortizacion.filtered(lambda l:  l.cuota_capital == 0 and l.programado == 0)
@@ -173,6 +190,9 @@ class WizardContratoAdendum(models.Model):
                 #                                        'estado_pago':a['estado_pago'],                                                
                 #                                            })
                 #crear el nuevo estado de cuenta 
+
+                cuota_adm_nueva=(nuevoCuotaAdm/int(intervalo_nuevo))
+                cuota_adm_nueva=round(cuota_adm_nueva, 2)
                 cuota_capital_nueva = (nuevoMontoReeestructura/int(intervalo_nuevo))
                 cuota_capital_nueva =round(cuota_capital_nueva, 2)
                 #raise ValidationError(str(cuota_capital_nueva)+'-- cuota_capital_nueva')
@@ -183,16 +203,17 @@ class WizardContratoAdendum(models.Model):
                     cuota_capital = (nuevoMontoReeestructura/int(intervalo_nuevo))
                     cuota_capital =round(cuota_capital, 2)
                     cuota_adm = self.monto_financiamiento * self.contrato_id.tasa_administrativa / 100 / 12
-                    iva = cuota_adm * 0.12
+                    iva = cuota_adm_nueva * 0.12
+
                     #monto_finan_contrato+= cuota_capital
                     cuota_asignada=i+1
                     cuota_administrativa_neto= cuota_adm + iva
-                    saldo = cuota_capital+cuota_adm+iva
+                    saldo = cuota_capital+cuota_adm_nueva+iva
                     self.env['contrato.estado.cuenta'].create({
                                                         'numero_cuota':i+1, 
                                                         'fecha':self.contrato_id.fecha_inicio_pago + relativedelta(months=i),
                                                         'cuota_capital':cuota_capital_nueva,
-                                                        'cuota_adm':cuota_adm,
+                                                        'cuota_adm':cuota_adm_nueva,
                                                         'iva_adm':iva,
                                                         'saldo':saldo,
                                                         'saldo_cuota_capital':cuota_capital,
@@ -259,6 +280,66 @@ class WizardContratoAdendum(models.Model):
 
 
                 ##########################################validar que la cuota capital este bien ####################################################
+                monto_adm_contrato=sum(self.contrato_id.tabla_amortizacion.mapped('cuota_adm'))
+                monto_adm_contrato= round(monto_adm_contrato,2)
+                cuota_admin_contrato=self.cuota_adm
+
+                vls_adm=[]
+                if  monto_adm_contrato  > cuota_admin_contrato:
+                    valor_sobrante = monto_adm_contrato - cuota_admin_contrato 
+                    valor_sobrante = round(valor_sobrante,2)
+                    parte_decimal, parte_entera = math.modf(valor_sobrante)
+                    #raise ValidationError('{0},{1},{2}'.format(valor_sobrante,parte_decimal,parte_entera))
+                    if parte_decimal==0:
+                        valor_a_restar=0
+                    elif parte_decimal >=1:
+                        valor_a_restar= (valor_sobrante/parte_decimal)*0.1
+                    else:
+                        valor_a_restar= (valor_sobrante/parte_decimal)*0.01
+                    #raise ValidationError('aaaaaaaaaaaaa{0}'.format(valor_a_restar)) 
+                    obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente'),('factura_id','=',False)] , order ='fecha desc')
+                    #raise ValidationError('aaaaaaaaaaaaa{0}'.format(obj_contrato))
+                    for c in obj_contrato:
+                        if valor_sobrante != 0.00 or valor_sobrante != 0 or valor_sobrante != 0.0:
+                            if c.programado == 0.00 or c.programado == 0 or c.programado == 0.0:
+                                c.update({
+                                    'cuota_adm': c.cuota_adm - valor_a_restar,
+                                    'contrato_id':self.contrato_id.id,
+                                })
+                                vls_adm.append(valor_sobrante)
+                                valor_sobrante = valor_sobrante -valor_a_restar
+                                valor_sobrante = round(valor_sobrante,2)
+                            
+                            
+                if  monto_adm_contrato  < cuota_admin_contrato:
+                    valor_sobrante = cuota_admin_contrato  - monto_adm_contrato 
+                    valor_sobrante = round(valor_sobrante,2)
+                    parte_decimal, parte_entera = math.modf(valor_sobrante)
+                    if parte_decimal==0:
+                        valor_a_restar=0
+                    elif parte_decimal >=1:
+                        valor_a_restar= (valor_sobrante/parte_decimal)*0.1
+                    else:
+                        valor_a_restar= (valor_sobrante/parte_decimal)*0.01
+
+                    obj_contrato=self.env['contrato.estado.cuenta'].search([('contrato_id','=',self.contrato_id.id),('estado_pago','=','pendiente'),('factura_id','=',False)] , order ='fecha desc')
+
+                    for c in obj_contrato:
+                        if valor_sobrante != 0.00 or valor_sobrante != 0 or valor_sobrante != 0.0:
+                            if c.programado == 0.00 or c.programado == 0 or c.programado == 0.0:
+                            #raise ValidationError(str(valor_sobrante)+'--'+str(parte_decimal)+'----'+str(valor_a_restar))
+                                c.update({
+                                    'cuota_adm': c.cuota_adm + valor_a_restar,
+                                    'contrato_id':self.contrato_id.id,
+                                })  
+                                vls_adm.append(valor_sobrante)
+                                valor_sobrante = valor_sobrante -valor_a_restar
+                                valor_sobrante = round(valor_sobrante,2)
+
+                valor_sobrante=0
+                valor_a_restar=0
+                parte_decimal=0
+                parte_entera=0
                 monto_finan_contrato = sum(self.contrato_id.tabla_amortizacion.mapped('cuota_capital'))
                 monto_finan_contrato = round(monto_finan_contrato,2)
                 monto_financiamiento_contrato=self.monto_financiamiento
