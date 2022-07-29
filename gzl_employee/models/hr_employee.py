8d15262162fa51d06bb7d6f7eb111d68153e276c
daylin2022 <melinarosado1996daylin@gmail.com>
2022-07-29 10:48:22 -0500

    Coreecion

--- gzl_employee/models/hr_employee.py
+++ gzl_employee/models/hr_employee.py
@@ -69,7 +69,7 @@ class HrEmployee(models.Model):
             comisiones_mes=comisiones_ids.filtered(lambda l: date.year == hoy.year and m.date.month == hoy.month and l.employee_id==x.employee_id.id)
             for m in comisiones_mes:
                 nominas_ids=self.env['hr.payslip'].search([('employee_id','=',m.employee_id.id)])
-                nomina_mes=nominas_ids.filtered(lambda l: l.date_from >= m.date and l.date_to <= m.date and l.payslip_run_id.type_payroll == 'bi-monthly')
+                nomina_mes=nominas_ids.filtered(lambda l: l.date_from >= m.date and l.date_to <= m.date and l.date_to.day==15 and l.payslip_run_id.type_payroll == 'bi-monthly')
                 if len(nomina_mes)==0:
                     self.envio_correos_plantilla('email_comisiones_pendientes',m.employee_id.id)
 
