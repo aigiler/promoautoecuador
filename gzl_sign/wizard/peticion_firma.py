c53a3e6e7b6fd07ce880cc43ac13ce7427d93fda
daylin2022 <melinarosado1996daylin@gmail.com>
2022-08-09 10:54:46 -0500

    Coreecion

--- gzl_sign/wizard/peticion_firma.py
+++ gzl_sign/wizard/peticion_firma.py
@@ -15,7 +15,7 @@ class SignSendRequest(models.TransientModel):
             if l.contrato:
                 grupo=l.contrato.grupo.id
                 lista_ids.append(l.contrato.cliente.id)
-        #self.signer_ids=lista_ids
+            self.update({'signer_ids':[(6,0,lista_ids)]}) 
             l.grupo=grupo 
 
     def sign_directly_without_mail(self):
