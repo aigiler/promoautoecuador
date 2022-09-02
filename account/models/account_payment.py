1c2c7c8cac8ffbc6e6efd69dcd96913edf038403
daylin2022 <melinarosado1996daylin@gmail.com>
2022-09-02 12:00:35 -0500

    Respaldos

--- account/models/account_payment.py
+++ account/models/account_payment.py
@@ -1105,7 +1105,7 @@ class account_payment(models.Model):
                         for fact in rec.invoice_ids:
                             monto_a_factura=0
                             valor_inicial_factura=fact.amount_residual
-                            movimiento_ids=moves.move_line_ids.filtered(lambda move: move.credit== self.valor_deuda)
+                            movimiento_ids=moves.line_ids.filtered(lambda move: move.credit== self.valor_deuda)
                             movimientos=(movimiento_ids + fact).line_ids.filtered(lambda line: not line.reconciled and line.account_id == rec.destination_account_id and not (line.account_id == line.payment_id.writeoff_account_id and line.name == line.payment_id.writeoff_label))
                             if movimientos:
                                 movimientos.reconcile()
