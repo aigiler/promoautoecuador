f652616f0a4a4b93ed143ba8a8cee28e7c3c8671
daylin2022 <melinarosado1996daylin@gmail.com>
2022-09-15 14:05:58 -0500

    Anticipo

--- l10n_ec_check_printing/models/account_payment.py
+++ l10n_ec_check_printing/models/account_payment.py
@@ -627,7 +627,7 @@ class AccountPayment(models.Model):
         #    self.credito=0
 
         else:
-            raise ValidationError("Comuniquese con el administrador del Sistema")
+            raise ValidationError("Comuniquese con el administrador del Sistema{0} ".format(pago_deuda))
         if self.credito==0.00:
             self.credito_contrato=False
             for registro in self.account_payment_account_ids:
