c49fcfc885dfeb8a9a9cf491a4774c072191460c
daylin2022 <melinarosado1996daylin@gmail.com>
2022-09-28 11:09:31 -0500

    Email

--- gzl_adjudicacion/models/devolucion_monto_vehiculo.py
+++ gzl_adjudicacion/models/devolucion_monto_vehiculo.py
@@ -447,7 +447,7 @@ class DevolucionMonto(models.Model):
     def validarrol(self):
         roles=self.env['adjudicaciones.team'].search([('id','=',self.rolAsignado.id)])
         for x in roles:
-          if self.env.user == x.user_id.id:
+          if self.env.user.id == x.user_id.id:
             return True
           else:
             raise ValidationError("Debe estar asignado al rol %s"% self.rolAsignado.name)
