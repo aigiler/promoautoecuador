5008124d91d925b7518f12f70d6a315d0cba55ca
daylin2022 <melinarosado1996daylin@gmail.com>
2022-10-07 22:38:53 -0500

    Entrega de Vehiculo

--- gzl_adjudicacion/models/entrega_vehiculo.py
+++ gzl_adjudicacion/models/entrega_vehiculo.py
@@ -9,6 +9,39 @@ from odoo.exceptions import UserError, ValidationError
 from dateutil.relativedelta import relativedelta
 
 
+
+class Partner(models.Model):   
+    _inherit = 'res.partner'    
+  
+
+
+
+    tipo=fields.Char(string='Tipo')
+    monto = fields.Float(string='Monto')
+    id_cliente = fields.Char(string='ID Cliente')
+    direccion = fields.Text(string='Dirección')
+    tipo_contrato = fields.Many2one("tipo.contrato.adjudicado", String="Tipo de Contrato")
+    codigo_cliente = fields.Char(string='Código Cliente')
+    fecha_nacimiento  = fields.Date(string='Fecha de nacimiento')
+    estado_civil = fields.Selection(selection=[
+                    ('soltero', 'Soltero/a'),
+                    ('union_libre', 'Unión libre'),
+                    ('casado', 'Casado/a'),
+                    ('divorciado', 'Divorciado/a'),
+                    ('viudo', 'Viudo/a')                    
+                    ], string='Estado Civil', default='soltero')
+    num_cargas_familiares = fields.Integer(string='Cargas Familiares')
+    comisionFacturaConcesionario = fields.Float(string="COMISION FACTURA A NOMBRE CONCESIONARIO")
+
+    conyuge=fields.Char(string='Nombre del Conyuge')
+
+    fechaNacimientoConyuge = fields.Date(string='Fecha de Nacimiento Conyuge')
+    vatConyuge = fields.Char(string='Cedula de Ciudadanía Conyuge')
+    direccion_trabajo=fields.Char(string='Dirección Laboral')
+    nombre_compania=fields.Char(string='Compañia')
+    telefono_trabajo=fields.Char(string='Telefono')
+    cargo=fields.Char(string='Cargo')
+    
 class EntegaVehiculo(models.Model):
     _name = 'entrega.vehiculo'
     _description = 'Entrega Vehiculo'
