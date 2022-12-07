e85fe12d2b1f8bf7dd17597697682e7ee48ef2ab
daylin2022 <melinarosado1996daylin@gmail.com>
2022-12-07 15:16:43 -0500

    Actualizacion de plantillas

--- gzl_reporte/wizard/__init__.py
+++ gzl_reporte/wizard/__init__.py
@@ -2,6 +2,7 @@
 from . import reporte_estado_de_cuenta_wizard
 from . import carta_finalizacion_wizard
 from . import congelamiento_wizard
+from . import cesion_derecho_wizard
 from . import contrato_adendum_wizard
 from . import contrato_reserva_wizard
 from . import hoja_ruta_wizard
