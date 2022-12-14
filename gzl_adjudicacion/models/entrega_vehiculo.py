dc51f955495dfa7b7911bb2be09c8cf422c02635
daylin2022 <melinarosado1996daylin@gmail.com>
2022-12-14 12:44:45 -0500

    Perfil garante

--- gzl_adjudicacion/models/entrega_vehiculo.py
+++ gzl_adjudicacion/models/entrega_vehiculo.py
@@ -1166,7 +1166,7 @@ class EntegaVehiculo(models.Model):
 
 
     def create_entrevista_garante(self):
-        plantilla_id=self.env['entrevista.credito'].create({'clave':"liquidacion_compra",
+        plantilla_id=self.env['entrevista.credito'].create({'clave':"entrevista_adjudicado",
                                                     'entrega_vehiculo_id':self.id})
         dct=plantilla_id.crear_formato_entrevista()
         return dct
