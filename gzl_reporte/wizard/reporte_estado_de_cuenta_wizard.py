c712de1115837704c23c6d1ef1d4497d9fccbed9
daylin2022 <melinarosado1996daylin@gmail.com>
2023-04-10 15:46:46 -0500

    Actualizacion estado de cuenta

--- gzl_reporte/wizard/reporte_estado_de_cuenta_wizard.py
+++ gzl_reporte/wizard/reporte_estado_de_cuenta_wizard.py
@@ -198,7 +198,7 @@ class ReporteEstadoDeCuentaIndividual(models.TransientModel):
         for col, head in enumerate(title_main):
             sheet.set_column('{0}:{0}'.format(chr(col + ord('A'))),6)
             sheet.write(14, col, head.upper(), formato_cabecera_tabla)
-        sheet.set_column('A:A',5)
+        sheet.set_column('A:A',6)
 
 
         line = itertools.count(start=15)
