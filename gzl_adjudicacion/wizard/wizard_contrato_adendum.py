269b81c634e07e9a0a8ca5ffa76c9f93e9d4bd2f
daylin2022 <106541416+daylin2022@users.noreply.github.com>
2023-05-19 15:44:51 -0500

    Update wizard_contrato_adendum.py

--- gzl_adjudicacion/wizard/wizard_contrato_adendum.py
+++ gzl_adjudicacion/wizard/wizard_contrato_adendum.py
@@ -314,6 +314,7 @@ class WizardContratoAdendum(models.Model):
                 else:
                     valor_a_restar= (valor_sobrante/parte_decimal)*0.01
                 obj_contrato=self.env['tabla.adendum'].search([('adendum_id','=',self.id),('estado_pago','=','pendiente'),('procesado','=',False)] , order ='fecha desc')
+                raise ValidationError(" {0}{1}{2}{3}  ".format(valor_a_restar,monto_adm_contrato,cuota_admin_contrato))
                 for c in obj_contrato:
                     if valor_sobrante != 0.00 or valor_sobrante != 0 or valor_sobrante != 0.0:
                         c.update({
